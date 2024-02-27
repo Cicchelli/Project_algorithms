54a044715d9dfe8a7d859db5e88a3506c55098d5
Stephan <cicchelli.stephan@gmail.com>
2024-02-27 20:58:13 -0300

    Req2

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,14 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError):
+        encrypt_message("message", "key")
+        encrypt_message(123, 4)
+
+    assert encrypt_message("message", 10) == "egassem"
+    assert encrypt_message("message", 3) == "sem_egas"
+    assert encrypt_message("abcdef", 4) == "fe_dcba"
+    assert encrypt_message("message", 5) == "assem_eg"
+    assert encrypt_message("message", 7) == "egassem"
