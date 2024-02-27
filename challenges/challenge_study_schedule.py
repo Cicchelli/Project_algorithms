60e72c8acc76e80af9edce2aac18efb375455491
Stephan <cicchelli.stephan@gmail.com>
2024-02-27 20:02:07 -0300

    Req1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,14 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if target_time is None or not all(
+        isinstance(period[0], int) and isinstance(period[1], int)
+        for period in permanence_period
+    ):
+        return None
+
+    students = sum(
+        1
+        for period in permanence_period
+        if period[0] <= target_time <= period[1]
+    )
+
+    return students
